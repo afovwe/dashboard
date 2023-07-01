71874cc4d66be90eb7430acc86cb066fb0ba4044
omofovwe Akpadaka <59265405+afovwe@users.noreply.github.com>
2023-07-01 22:39:11 +0100

    Port changed from 3000 to dynamic ||3000

--- src/server.js
+++ src/server.js
@@ -1030,4 +1030,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/realtor_exp_frontend/index.html'));
 }); 
 // start the server
-app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+//app.listen(process.env.PORT || 3000, () => console.log('Server started on port 3000'));
