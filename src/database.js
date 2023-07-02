aefed6bf8166cdffdff424f4c0dc728ad4f40681
omofovwe Akpadaka <59265405+afovwe@users.noreply.github.com>
2023-07-02 17:45:47 +0100

    Getdownlind CORS error fixed locally

--- src/database.js
+++ src/database.js
@@ -53,10 +53,10 @@ const db = {
 export { db };  */
 
 
-console.log('DB_USER:', process.env.DB_USER);
+/* console.log('DB_USER:', process.env.DB_USER);
 console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
 console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_DATABASE:', process.env.DB_DATABASE);
+console.log('DB_DATABASE:', process.env.DB_DATABASE); */
 
 const pool = mysql.createPool({
   user: process.env.DB_USER,
