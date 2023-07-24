20370d26252422c2b4dfa2187d6d2483cdcb7a26
omofovwe Akpadaka <59265405+afovwe@users.noreply.github.com>
2023-07-24 10:46:58 +0100

    To be pushed publicly

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 // Load environment variables from .env file
 dotenv.config();
-/*
+
  const pool = mysql.createPool({
   host: 'localhost',
   user: 'realtor',
@@ -17,23 +17,5 @@ dotenv.config();
 export const db = {
   query: (queryString, escapedValues) => pool.execute(queryString, escapedValues),
 };  
-*/
 
 
-/* console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_DATABASE:', process.env.DB_DATABASE); */ 
- 
-  const pool = mysql.createPool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,  
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-});
-
-//export default db;
-
-export const db = {
-  query: (queryString, escapedValues) => pool.execute(queryString, escapedValues),
-};   
